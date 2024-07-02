926b003fc1bccfbd5742a642193f85d18d9e6b2c
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-07-02 20:57:48 +0300

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Stack
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = "((()))";
+            Console.WriteLine(IsCorrect(pattern));
+            
+        }
+
+        public static bool IsCorrect(string str)
+        {
+            var stack = new Stack<char>();
+            foreach(var symbol in str)
+            {
+                switch(symbol)
+                {
+                    case '(':
+                        stack.Push(symbol);
+                        break;
+                    case ')':
+                        if(stack.Count == 0) return false;
+                        if (stack.Pop() != '(') return false;
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
